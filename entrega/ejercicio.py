c7ff4150da6a6200d4f1e867cb1526cae11b88d1
maxsiii925 <68820855+maxsiii925@users.noreply.github.com>
2023-10-23 19:58:11 +0000

    actual

--- entrega/ejercicio.py
+++ entrega/ejercicio.py
@@ -1,5 +1,8 @@
-print(22+33)
-print(22-33)
-print(22*33)
-print(33//22)
-print(22%33)
+nombre="Maximiliano"
+apellido="Medina"
+edad=28
+print(nombre,apellido)
+print('"dog"')
+print(id(edad))
+edad=edad+1
+print(edad)
